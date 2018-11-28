bda0e17263f97953b499d98bcc0573d6ffcd782e
Tu Nguyen <ladiesman9x@gmail.com>
2018-11-28 10:07:56 +0700

    Add react core files

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,11 @@
+var config = {
+    map: {
+        '*': {
+            react: 'Vnecoms_ReactBase/js/react',
+            reactDom: 'Vnecoms_ReactBase/js/react-dom'
+        }
+    },
+    shim: {
+
+    }
+};
